6d11a99fd0225b6d397201ce5be2c55adc4d2f3f
Lucas Melo <lucas.melo044@gmail.com>
2024-05-20 21:26:22 -0300

    feat: add ReprodutorMusical interface

--- src/application/Iphone.java
+++ src/application/Iphone.java
@@ -1,14 +1,36 @@
 package application;
+
+import interfaces.ReprodutorMusical;
 import interfaces.Telefone;
 
-public class Iphone implements Telefone{ 
-    public void ligar () {
-        System.out.println("Fazendo ligação");
-    } 
-    public void atender () {
-        System.out.println("Atendendo ligação");
-    } 
-    public void iniciarCorreioVoz () {
-        System.out.println("Iniciando correio de voz");
-    }
+public class Iphone implements Telefone, ReprodutorMusical {
+  @Override
+  public void ligar() {
+    System.out.println("Fazendo ligação");
+  }
+
+  @Override
+  public void atender() {
+    System.out.println("Atendendo ligação");
+  }
+
+  @Override
+  public void iniciarCorreioVoz() {
+    System.out.println("Iniciando correio de voz");
+  }
+
+  @Override
+  public void tocar() {
+    System.out.println("Tocando música");
+  }
+
+  @Override
+  public void pausar() {
+    System.out.println("Pausando música");
+  }
+
+  @Override
+  public void selecionarMusica() {
+    System.out.println("Selecionando música");
+  }
 }
